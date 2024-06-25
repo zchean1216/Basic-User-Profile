e8fde8c5ea23c14758d88978df0429cd850560f7
Zheng Chean Chia <zhengchean@gmail.com>
2024-06-26 03:45:46 +1000

    Add files via upload

---
+++ basic/src/App.vue
@@ -0,0 +1,98 @@
+<template>
+    <div v-show="!isEditMode">
+        <h1>User profile</h1>
+        <img :src="image">
+
+        <span>Name: </span><b id="name">{{ name }}</b>
+        <hr />
+
+        <span>Email: </span><b id="email">{{ email }}</b>
+        <hr />
+        
+        <span>Interests: </span><b id="interests">{{ interests }}</b>
+        <hr />
+
+        <button @click="handleEditProfile">Edit Profile</button>
+    </div>
+    <div v-show="isEditMode">
+        <h1>User profile</h1>
+        <img :src="image">
+
+        <span>Name: </span>
+        <input type="text" id="input-name" v-model="name" />
+        <hr />
+
+        <span>Email: </span>
+        <input type="text" id="input-email" v-model="email"/>
+        <hr />
+        
+        <span>Interests: </span>
+        <input type="text" id="input-interests" v-model="interests"/>
+        <hr />
+
+        <button @click="handleUpdateProfile">Update Profile</button>
+    </div>
+</template>
+
+<script>
+import image from "./IMG_2180.jpg"
+export default {
+name: "App",
+data() {
+    return {
+        image: image,
+        name: "Jayden Chia",
+        email:"zhengchean@gmail.com",
+        interests: "IT",
+        isEditMode: false
+
+    }
+},
+methods: {
+        handleEditProfile(){
+            this.isEditMode = true
+        },
+
+        handleUpdateProfile(){
+            this.isEditMode = false
+        }
+    }
+}
+</script>
+
+<style>
+    img {
+        width: 300px;
+        height: auto;
+        display: block;
+        margin-bottom: 40px;
+    }
+
+    div {
+        margin: 40px auto;
+        width: 80%
+    }
+
+    hr {
+        width: 400px;
+        margin: 25px 0;
+    }
+
+    button {
+        width: 160px;
+        font-size: 15px;
+        height: 45px;
+        border-radius: 5px;
+    }
+
+    button:hover {
+        cursor: pointer;
+    }
+
+    input{
+        width: 200px;
+        font-size: 15px;
+        padding: 10px;
+    }
+
+</style>
